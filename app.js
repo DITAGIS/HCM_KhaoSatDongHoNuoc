92b4cce7cfc7d2bbb842871b222556bea35b4b4e
fuhidev <fuhi.dev@gmail.com>
2018-01-21 22:47:49 +0700

    đăng nhập

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session')
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+var database = require('./modules/Database');
 var index = require('./routes/index');
 var app = express();
+app.set('view engine', 'ejs');
 
-// view engine setup
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(session({
+  secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
+  resave: true,
+  saveUninitialized: true,
+}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -20,6 +25,71 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
+
+
+
+//PASSPORT
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser((user, done) => {
+  console.log('serializeUser');
+  done(null, user);
+})
+passport.deserializeUser((user, done) => {
+  console.log('deserializeUser');
+  done(null, user);
+})
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    database.isUser(username, password)
+      .then(function (user) {
+        // bcrypt.compare(password, user.Password, function (err, result) {
+        if (!user) {
+          return done(null, false, {
+            message: 'Incorrect username and password'
+          });
+        }
+        return done(null, user);
+        // })
+      }).catch(function (err) {
+        return done(err);
+      })
+  }
+))
+app.get("/", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("index");
+  } else
+    res.redirect("/login")
+})
+app.get("/login", function (req, res) {
+    res.render("login")
+})
+app.post('/session', function (req, res) {
+  if (req.isAuthenticated())
+    res.status(200).send(req.session.passport.user);
+  else
+    res.status(400).send();
+})
+app.post('/login', passport.authenticate('local', {
+  failureRedirect: '/login',
+  failureFlash: true
+}), function (req, res) {
+  if (req.body.remember == "true") {
+    req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000; // Cookie expires after 30 days
+  } else {
+    req.session.cookie.expires = false; // Cookie expires at end of session
+  }
+  res.redirect('/')
+})
+app.get('/logout', (req, res) => {
+  res.clearCookie('passport');
+  req.session.destroy();
+  res.redirect('/');
+  res.end()
+})
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ modules/Database.js
@@ -0,0 +1,39 @@
+const config = {
+    user: 'khaosatdonghonuoc',
+    password: 'khaosatdonghonuoc',
+    server: 'ditagis.com',
+    database: 'BINHDUONG_KHAOSATDONGHONUOC',
+    options: {
+        encrypt: false // Use this if you're on Windows Azure 
+    }
+}
+const sql = require('mssql')
+function isUser(username, password) {
+    return new Promise((resolve, reject) => {
+        sql.connect(config).then(() => {
+            return sql.query`SELECT * FROM SYS_ACCOUNT WHERE USERNAME = ${username} AND PASSWORD = ${password}`;
+        }).then(result => {
+            if (result.recordset.length > 0)
+                resolve(result.recordset[0])
+            else resolve(null);
+            sql.close();
+        }).catch(err => {
+            console.log(err);
+            sql.close();
+        })
+    });
+}
+function danhBo(id){
+    sql.connect(config).then(() => {
+        return sql.query`SELECT MaDanhBo FROM DANHSACHMADANHBO WHERE MaDanhBo = '${id}'`;
+    }).then(result => {
+        if (result.recordset.length > 0)
+            resolve(result.recordset[0])
+        else resolve(null);
+        sql.close();
+    }).catch(err => {
+        console.log(err);
+        sql.close();
+    })
+}
+module.exports.isUser = isUser;
